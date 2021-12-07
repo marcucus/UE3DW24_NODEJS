efff510036d044a4541751cd081f11b563e40260
Adrien Marques <marquesadrien.pro@gmail.com>
2021-12-07 16:14:05 +0000

    Main : Fin

--- tests/user.test.js
+++ tests/user.test.js
@@ -14,7 +14,7 @@ describe('Test : Get all users', function() {
 describe('Test : Get one users', function() {
     it('responds with json', function(done) {
         request(app)
-            .get('/api/users/1')
+            .get('/api/user/1')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
             .expect(201, done);
